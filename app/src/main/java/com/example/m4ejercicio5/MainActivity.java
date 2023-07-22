6db4e623fd51f5a8a46556a83d2b8bc0c120af17
Alexis.Huaiquil <ing.ahuaiquil@gmail.com>
2023-07-22 11:59:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/m4ejercicio5/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.m4ejercicio5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initListeners();
+
+    }
+
+    private void initListeners() {
+        Button showToast = findViewById(R.id.buttonShowToast);
+        Button counter = findViewById(R.id.buttonCounter);
+        TextView textView = findViewById(R.id.textView);
+
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "hello toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer numero = Integer.parseInt(textView.getText().toString());
+                numero++;
+                textView.setText(numero.toString());
+
+            }
+        });
+
+    }
+}
